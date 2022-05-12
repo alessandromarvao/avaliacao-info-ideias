de36221614b8279d38a3c6e726387f132ac2f648
Alessandro Marvão <alessandromarvao@gmail.com>
2022-05-11 22:42:07 -0300

    Corrigindo e concluindo o quesito 4

--- primos/index.php
+++ primos/index.php
@@ -1,7 +1,28 @@
 <?php
+/**
+ * Retorna os números primos dentro do intervalo citado, desprezando os números citados.
+ */
 function primos(int $numInicial, int $numFinal) {
+    // Array que receberá os números primos do intervalo
     $array = [];
-    for ($i = $numInicial; $i<$numFinal; $i++) {
-        
+
+    // Percorre os itens dentro do intervalo citado
+    for ($i = $numInicial+1; $i<$numFinal; $i++) { 
+        // Presume que o número atual dentro do intervalo é primo
+        $bool = true;
+
+        // Percorre os números de 2 até o citado e confere se o resto da divisão dentre cada iteração é igual a zero
+        for($j = 2; $j<$i; $j++){
+            if($i % $j == 0) {
+                // O número não é primo
+                $bool = false; 
+                break;
+            }
+        }
+        if($bool) { // confere se o número é primo e armazena no array
+            array_push($array, $i);
+        }
     }
+
+    return $array;
 }
--- sequencia-crescente/index.php
+++ sequencia-crescente/index.php
@@ -1,14 +1,23 @@
 <?php
 
-function sequenciaCrescente(array $array){
+function sequenciaCrescente(array $array)
+{
     $bool = false;
 
-    foreach($array as $row1) {
-        for($i=0; $i<count($array); $i++) {
-            
-        }
-    }
+    for ($i = 0; $i < count($array); $i++) {
 
+        $novo_array = [];
+
+        for ($j = 0; $j < count($array); $j++) {            
+            if ($i != $j) {
+                array_push($novo_array, $array[$j]);
+            }
+        };
+
+        echo "<pre>";
+        print_r($novo_array);
+        echo "</pre>";
+    }
 }
 
-echo sequenciaCrescente([1,2,3,1,1,5]);
+echo sequenciaCrescente([1, 2, 3, 1, 1, 5]);
